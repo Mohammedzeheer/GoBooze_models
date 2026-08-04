91d108bc8cb703e0be11b0a122c574d0087402ea
Mohammedzeheer <zeheerzak@gmail.com>
2026-08-04 18:43:36 +0530

    tasting needs and group tag added

--- product.js
+++ product.js
@@ -23,6 +23,11 @@ const productSchema = new mongoose.Schema(
       ref: "brands",
       required: true,
     },
+    groupTag: {
+      type: ObjectId,
+      ref: "GroupTag",
+      required: false,
+    },
     tags: {
       type: Array,
       default: [],
@@ -54,113 +59,11 @@ const productSchema = new mongoose.Schema(
       default: "active",
       required: true,
     },
-
-    // product_name: {
-    //   type: String,
-    //   required: true,
-    // },
-    // product_code: {
-    //   type: String,
-    //   required: false,
-    // },
-    // product_description: {
-    //   type: String,
-    //   required: false,
-    // },
-    // collection: {
-    //   type: ObjectId,
-    //   ref: "Category",
-    //   required: true,
-    // },
-    // item_id: {
-    //   type: ObjectId,
-    //   ref: "Item",
-    //   required: true,
-    // },
-    // sub_collection: {
-    //   type: ObjectId,
-    //   ref: "SubCollection",
-    //   required: false,
-    // },
-    // selling_price: {
-    //   type: Number,
-    //   required: true,
-    // },
-    // cost_price: {
-    //   type: Number,
-    //   required: true,
-    // },
-    // gross_profit: {
-    //   type: Number,
-    //   required: false,
-    // },
-    // quantity: {
-    //   type: Number,
-    //   required: true,
-    // },
-    // discount: {
-    //   type: Number,
-    //   required: false,
-    //   default: 0,
-    // },
-
-    // average_rating: {
-    //   type: Number,
-    //   required: false,
-    //   default: 0,
-    // },
-    // number_of_ratings: {
-    //   type: Number,
-    //   required: false,
-    //   default: 0,
-    // },
-    // number_of_reviews: {
-    //   type: Number,
-    //   required: false,
-    //   default: 0,
-    // },
-    // number_of_times_ordered: {
-    //   type: Number,
-    //   required: false,
-    //   default: 0,
-    // },
-    // in_favourites: {
-    //   type: Number,
-    //   required: false,
-    //   default: 0,
-    // },
-    // in_customer_cart: {
-    //   type: Number,
-    //   required: false,
-    //   default: 0,
-    // },
-    // is_popular: {
-    //   type: Boolean,
-    //   required: false,
-    //   default: false,
-    // },
-    // tags: {
-    //   type: [String],
-    //   required: false,
-    // },
-
-    // added_by: {
-    //   type: ObjectId,
-    //   ref: "Admin",
-    //   required: true,
-    // },
-    // updated_by: {
-    //   type: ObjectId,
-    //   ref: "Admin",
-    //   required: false,
-    // },
-
-    // status: {
-    //   type: String,
-    //   enum: ["active", "inactive"],
-    //   default: "active",
-    //   required: true,
-    // },
+    tastingNotes: {
+      type: String,
+      required: false,
+      default: "",
+    },
   },
   {
     suppressReservedKeysWarning: true,
