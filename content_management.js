9215898c30254f0db1a06349dd75060fb28b0ef5
Mohammedzeheer <zeheerzak@gmail.com>
2026-08-05 13:01:24 +0530

    enum added

--- content_management.js
+++ content_management.js
@@ -19,6 +19,7 @@ const contentManagementSchema = new mongoose.Schema(
         "trade_banner",
         "advertisement_banner",
         "home_hero_banner",
+        "category_hero_banner"
       ],
     },
     title: {
